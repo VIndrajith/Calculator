4aff0db6ad3fc912c5ca1f749308e26721f6cca6
VIndrajith <130754879+VIndrajith@users.noreply.github.com>
2023-04-15 21:43:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,11 @@
-const display = document.getElementById('result');
-const clearButton = document.getElementById('clear');
-const equalsButton = document.getElementById('equals');
-const operatorButtons = document.querySelectorAll('.operator');
-const numberButtons = document.querySelectorAll('[value]');
-
-let currentOperand = '';
-let previousOperand = '';
-let currentOperator = null;
-
-function updateDisplay() {
-  display.innerText = currentOperand;
+var screen = document.getElementById('screen');
+function buttonclick(value){
+    screen.value +=value;
 }
-
-function clear() {
-  currentOperand = '';
-  previousOperand = '';
-  currentOperator = null;
-  updateDisplay();
+function  clearscreen(){
+    screen.value = "";
 }
-
-function appendNumber(number) {
-  if (number === '.' && currentOperand.includes('.')) return;
-  currentOperand += number;
-  updateDisplay();
+function findresult(){
+    var result = eval(screen.value)
+    screen.value = result;
 }
-
-function chooseOperator(operator) {
-  if (currentOperand === '') return;
-  if (previousOperand !== '') {
-    compute();
-  }
-  currentOperator = operator;
-  previousOperand = currentOperand;
-  currentOperand = '';
-}
-
-function compute() {
-  let computation;
-  const prev = parseFloat(previousOperand);
-  const current = parseFloat(currentOperand);
-  if (isNaN(prev) || isNaN(current)) return;
-  switch (currentOperator) {
-    case '+':
-      computation = prev + current;
-      break;
-    case '-':
-      computation = prev - current;
-      break;
-    case '*':
-      computation = prev * current;
-      break;
-    case '/':
-      computation = prev / current;
-      break;
-    default:
-      return;
-  }
-  currentOperand = computation.toString();
-  currentOperator = null;
-  previousOperand = '';
-  updateDisplay();
-  document.getElementById("myDiv").style.borderStyle = "solid"
-}
-
-clearButton.addEventListener('click', clear);
-
-equalsButton.addEventListener('click', function() {
-  if (currentOperator === null) return;
-  compute();
-});
-
-numberButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      appendNumber(button.value);
-
-operatorButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    chooseOperator(button.value);
-  
-  });
-
-
-    });
-  });
-});
-
-
-    
